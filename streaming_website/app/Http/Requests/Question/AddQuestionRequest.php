c04130afaa3bd3ef83116ed70865fbd39ff856d8
benbasse <moussabasse221@gmail.com>
2024-04-18 19:14:00 +0000

    question request

--- streaming_website/app/Http/Requests/Question/AddQuestionRequest.php
+++ streaming_website/app/Http/Requests/Question/AddQuestionRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Question;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AddQuestionRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class AddQuestionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,27 @@ class AddQuestionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'contenue' => 'required|string',
+            'categorie_id' => 'required'
         ];
     }
+
+    public function failedValidation(Validator $validator ){
+        throw new HttpResponseException(response()->json([
+            'success'=>false,
+            'status_code'=>422,
+            'error'=>true,
+            'message'=>'erreur de validation',
+            'errorList'=>$validator->errors()
+        ]));
+    }
+
+    public function messages(){
+        return [
+            'contenue.required' => 'le contenue ne peut pas etre null', 
+            'contenue.string' => 'le contenue doit etre de type text',
+            'categorie_id' => 'la categorie est requis'
+        ];
+    }
+
 }
