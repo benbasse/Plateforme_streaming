cc762d446c1c97dc30d5333c9410a2bedd0d197b
benbasse <moussabasse221@gmail.com>
2024-04-19 12:03:44 +0000

    Controller pour les reponses

--- streaming_website/app/Http/Controllers/ReponseController.php
+++ streaming_website/app/Http/Controllers/ReponseController.php
@@ -2,64 +2,113 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Reponse\AddReponseRequest;
+use App\Http\Requests\Reponse\EditReponseRequest;
+use App\Models\Question;
 use App\Models\Reponse;
+use Exception;
 use Illuminate\Http\Request;
-
+use App\Traits\ApiResponseTrait as ApiResponseTrait;
 class ReponseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    use ApiResponseTrait;
     public function index()
     {
-        //
+        try {
+            $reponse = Reponse::with('Question')->get();
+            if ($reponse->isEmpty()) {
+                return $this->errorResponse("La liste des reponses est vide");
+            } else {
+                return $this->succesResponse($reponse, "Tous les reponses");
+            }
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+
+    public function store(AddReponseRequest $request)
     {
-        //
+        try {
+            $reponse = new Reponse();
+            $reponse->contenue = $request->contenue;
+            $question = Question::find($request->question_id);
+            if ($question) {
+                $reponse->question_id = $question->id;
+                if ($reponse->save()) {
+                    return $this->succesResponse($reponse, "Reponse enregistre");
+                } else {
+                    return $this->errorResponse("Reponse non enregistre");
+                }
+            } else {
+                return $this->errorResponse("Cette question n'existe pas");
+            }
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+
+    public function show($id)
     {
-        //
+        try {
+            $reponse = Reponse::find($id);
+            if ($reponse) {
+                return $this->succesResponse($reponse,"Details reponse");
+            } else {
+                return $this->errorResponse("Reponse n'existe pas");
+            }
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Reponse $reponse)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Reponse $reponse)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Reponse $reponse)
+    public function update(EditReponseRequest $request, $id)
     {
-        //
+        try {
+            $reponse = Reponse::find($id);
+            if ($reponse) {
+                $reponse->contenue = $request->contenue;
+                if ($question = Question::find($request->question_id)) {
+                    $reponse->question_id = $question->id;
+                    if ($reponse->save()) {
+                        return $this->succesResponse($reponse,"Reponse modifier avec succes");
+                    } else {
+                        return $this->errorResponse("reponse non modifie");
+                    }
+                } else{
+                    return $this->errorResponse("Cette question n'existe pas");
+                }
+            } else{
+                return $this->errorResponse("Cette reponse n'existe pas");
+            }
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Reponse $reponse)
+    public function destroy($id)
     {
-        //
+        try {
+            $reponse = Reponse::find($id);
+            if ($reponse) {
+                $reponse->delete();
+                return response()->json([
+                    "status" => 200,
+                    "message" => "reponse supprimée",
+                ]);
+            } else {
+                return $this->errorResponse("reponse non trouve");
+            }
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
     }
 }
