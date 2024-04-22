229667fb42f5a241fde639b4c5bce5dd04899fba
benbasse <moussabasse221@gmail.com>
2024-04-22 12:59:24 +0000

    Modification de la methode inscription

--- streaming_website/app/Http/Controllers/AuthController.php
+++ streaming_website/app/Http/Controllers/AuthController.php
@@ -9,6 +9,8 @@ use Exception;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
 
 class AuthController extends Controller
 {
@@ -87,30 +89,76 @@ class AuthController extends Controller
         ]);
     }
 
-    public function inscription(RegisterUserRequest $request)
-    {
-        try {
-            $user = new User();
-            $user->nom = $request->nom;
-            $user->prenom = $request->prenom;
-            $user->image = $this->storeImage($request->image);
-            $user->telephone = $request->telephone;
-            $user->email = $request->email;
-            $user->password = Hash::make($request->password);
-            $user->save();
-            return response()->json([
-                'status_code' => 200,
-                'status_message' => 'inscription reussi'
-            ]);
-        } catch (Exception $e) {
-            return response()->json($e);
-        }
-    }
+    // public function inscription(RegisterUserRequest $request)
+    // {
+    //     try {
+    //         $user = new User();
+    //         $user->nom = $request->nom;
+    //         $user->prenom = $request->prenom;
+    //         $user->image = $this->storeImage($request->image);
+    //         $user->telephone = $request->telephone;
+    //         $user->email = $request->email;
+    //         $user->password = Hash::make($request->password);
+    //         $user->save();
+    //         return response()->json([
+    //             'status_code' => 200,
+    //             'status_message' => 'inscription reussi'
+    //         ]);
+    //     } catch (Exception $e) {
+    //         return response()->json($e);
+    //     }
+    // }
 
     private function storeImage($image)
     {
         return $image->store('user', 'public');
     }
-
+    public function inscription(Request $request)
+    {
+            $existingUser = User::where('email', $request->email)->exists();
+    
+            if ($existingUser) {
+                $user = User::where('email', $request->email)->first();
+                return response()->json([
+                    "status" => 200,
+                    "message" => "Les informations liées à cet email",
+                    "user" => $user
+                ]);
+            } else {
+                $validatedData = $this->validate($request, [
+                    'nom'=>'required',
+                    'prenom'=>'required',
+                    'image'=>'required',
+                    'email'=>'required|unique:users,email|email',
+                    'password'=>'required|regex:/^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[@#$%^&+=!])(.{8,})$/',
+                    'telephone' =>'required|regex:/^7[0-9]{8}$/|unique:users,telephone',
+                ],[
+                    'nom.required'      =>  'Le nom est requis',
+                    'prenom.required'   =>  'Le prénom est requis',
+                    'image.required'    =>  'L\'image est requise',
+                    'email.required'    =>  'L\'email est requis',
+                    'email.unique'      =>  'L\'email existe déjà',
+                    'email.email'       =>  "Format d'email incorrect",
+                    'password.required' =>  'Le mot de passe est requis',
+                    'password.regex'    =>  "Le mot de passe doit contenir au moins 8 caractères avec un chiffre, une lettre et un caractère spécial",
+                    'telephone.required'=>  'Le numéro de téléphone est requis',
+                    'telephone.unique'  =>  'Le numéro de téléphone est déjà utilisé',
+                    'telephone.regex'   =>  'Le format du numéro de téléphone est incorrect',
+                ]);
+                $user = new User();
+                $user->nom = $validatedData['nom'];
+                $user->prenom = $validatedData['prenom'];
+                $user->image = $this->storeImage($validatedData['image']);
+                $user->telephone = $validatedData['telephone'];
+                $user->email = $validatedData['email'];
+                $user->password = Hash::make($validatedData['password']);
+                $user->save();
+                return response()->json([
+                    'status_code' => 200,
+                    'status_message' => 'Inscription réussie'
+                ]);
+            }
+    }
+    
 
 }
