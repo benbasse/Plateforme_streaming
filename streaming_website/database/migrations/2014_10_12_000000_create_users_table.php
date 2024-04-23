d293cfdf0a618a03dbcbd993eca7c16bd0c55fda
benbasse <moussabasse221@gmail.com>
2024-04-23 16:26:58 +0000

    Auth controller modification de l'enregistrement des utiliateurs

--- streaming_website/database/migrations/2014_10_12_000000_create_users_table.php
+++ streaming_website/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('nom');
             $table->string('prenom');
-            $table->mediumText('image');
             $table->integer('telephone')->unique();
             $table->enum('role', ['user', 'admin'])->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default('B@sse@bdou221');
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
